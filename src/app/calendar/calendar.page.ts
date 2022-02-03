715a5761efaf5a1e27b6c0ffc278c131dd5cb70f
Graham Naquin <graham@naquin.live>
2022-02-03 10:49:57 -0600

    done as much as I can without mari input and information

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.page.scss'],
 })
 export class CalendarPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  contactButton() {
+    console.log('contact button clicked');
+    window.location.href =
+      'https://docs.google.com/forms/d/e/1FAIpQLSdWPif5D-Ige00RHWZ4w2t9YB-i9azPhQu17xgf--Ov4q8DAg/viewform?usp=sf_link';
   }
-
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,23 @@ export class HomePage {
 
   constructor() {}
 
+  contactButton() {
+    console.log('contact button clicked');
+    window.location.href =
+      'https://docs.google.com/forms/d/e/1FAIpQLSdWPif5D-Ige00RHWZ4w2t9YB-i9azPhQu17xgf--Ov4q8DAg/viewform?usp=sf_link';
+  }
+
+  calendarButton() {
+    console.log('calendar button clicked');
+    window.location.href = 'http://localhost:8100/calendar';
+  };
+
+  donateButton() {
+    console.log('donate button clicked');
+  };
+
+  registerButton() {
+    console.log('register button clicked');
+  };
+
 }
