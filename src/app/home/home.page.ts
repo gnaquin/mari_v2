b3b7f3e56e69992713630a46c85661c6c843d182
Graham Naquin <grahamnaquin@Grahams-MacBook-Pro.local>
2022-02-15 15:36:17 -0600

    preparing for deployment, real routing added to relevent buttons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,11 +13,6 @@ export class HomePage {
     console.log('contact button clicked');
     window.location.href =
       'https://docs.google.com/forms/d/e/1FAIpQLSdWPif5D-Ige00RHWZ4w2t9YB-i9azPhQu17xgf--Ov4q8DAg/viewform?usp=sf_link';
-  }
-
-  calendarButton() {
-    console.log('calendar button clicked');
-    window.location.href = 'http://localhost:8100/calendar';
   };
 
   donateButton() {
